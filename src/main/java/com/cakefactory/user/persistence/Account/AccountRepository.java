1f503d1d1eb33ad009553e06d804014964e8becc
JoJaWiley <joshua.wiley@summitoic.edu>
2023-11-27 20:08:13 -0600

    Fleshed out JpaAccountService without using project hints.

--- src/main/java/com/cakefactory/user/persistence/Account/AccountRepository.java
+++ src/main/java/com/cakefactory/user/persistence/Account/AccountRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.Optional;
 
 public interface AccountRepository extends CrudRepository<AccountEntity, String> {
-    Optional<AccountEntity> findByEmail(String email);
+    AccountEntity findByEmail(String email);
 }
